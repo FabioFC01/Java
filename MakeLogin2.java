23c9b5c604b81462b7153a105a300454bc1b35a3
Fabio Cociancich <fcociancich@gmail.com>
2020-10-15 09:29:45 +0200

    Add files via upload

---
+++ MakeLogin2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MakeLogin2{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserire il nome : ");
+        String nome = in.next();
+
+        System.out.print("Inserire il cognome : ");
+        String cognome = in.next();
+
+        System.out.print("Inserire la matricola : ");
+        int matr = in.nextInt();
+
+        String inits = nome.substring(0,1) + cognome.substring(0,1);
+        String lg = inits.toLowerCase() + matr;
+
+        System.out.println("La login e' : " + lg);
+
+        in.close();
+    }
+}
